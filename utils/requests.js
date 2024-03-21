2dd5720a010aa9ada8cadf825bfff253b9d1c4bc
dnmckin <tradeelite@protonmail.com>
2024-03-20 22:54:39 -0400

    change request dictionary to have string keys

--- utils/requests.js
+++ utils/requests.js
@@ -1,53 +1,53 @@
 const API_KEY = process.env.API_KEY;
 
 export default {
-    fetchTrending: {
+    "fetchTrending": {
         title: "Trending",
         url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
     },
-    fetchTopRated: {
+    "fetchTopRated": {
         title: "Top Rated",
         url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`
     },
-    fetchActionMovies: {
+    "fetchActionMovies": {
         title: "Action",
         url: `/discover/movie?api_key=${API_KEY}&with_genres=28`
     },
-    fetchComedyMovies: {
+    "fetchComedyMovies": {
         title: "Comedy",
         url: `/discover/movie?api_key=${API_KEY}&with_genres=35`
     },
-    fetchHorrorMovies: {
+    "fetchHorrorMovies": {
         title: "Horror",
         url: `/discover/movie?api_key=${API_KEY}&with_genres=27`
 
     },
-    fetchRomanceMovies: {
+    "fetchRomanceMovies": {
         title: "Romance",
         url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`
 
     },
-    fetchMystery: {
+    "fetchMystery": {
         title: "Mystery",
         url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`
 
     },
-    fetchSciFi: {
+    "fetchSciFi": {
         title: "Sci-Fi",
         url: `/discover/movie?api_key=${API_KEY}&with_genres=878`
 
     },
-    fetchWestern: {
+    "fetchWestern": {
         title: "Western",
         url: `/discover/movie?api_key=${API_KEY}&with_genres=37`
 
     },
-    fetchAnimation: {
+    "fetchAnimation": {
         title: "Animation",
         url: `/discover/movie?api_key=${API_KEY}&with_genres=16`
 
     },
-    fetchTV: {
+    "fetchTV": {
         title: "TV Movie",
         url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`
 
