66dab90c9c162a227833301eb6b22fe288dd2363
dnmckin <tradeelite@protonmail.com>
2024-03-22 01:27:22 -0400

    add types to search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,24 +1,24 @@
-import React from "react"
+import React, { ChangeEvent } from "react"
 import { useState, useEffect, SyntheticEvent } from "react"
-// import StandardLayout from "@/components/layout"
 
 import Header from "../components/Header"
 import Results from "../components/Results"
+import { ResultItem } from "@/components/Thumbnail"
 import {getMovieData} from "../utils/searchapi"
 import {MagnifyingGlassIcon} from "@heroicons/react/24/outline"
 import {useRouter} from "next/router"
 
-export const Search = ({results}) => {
+export const Search = (props :{results : Array<ResultItem>}) => {
   const [domLoaded, setDomLoaded] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState<Array<ResultItem>>([]);
 
-
+  const {results} = props;
   const router = useRouter();
   let timer:any = null;
   
 
-  const onSearch = (event:SyntheticEvent) => {
+  const onSearch = (event:ChangeEvent<HTMLInputElement>) => {
     clearTimeout(timer);
 
     const q = event.target?.value;
@@ -27,7 +27,7 @@ export const Search = ({results}) => {
     timer = setTimeout(()=> {runSearch(q)}, 1000)
   }
 
-  const runSearch = async (q) => {
+  const runSearch = async (q:string) => {
     if(q.length >= 3 || q.length == 0){
       router.push(`./search?q=${q}`)      
     }
@@ -65,7 +65,7 @@ export const Search = ({results}) => {
      )
 }
 
-export const getServerSideProps =  async (props) => {
+export const getServerSideProps =  async (props:any) => {
   const data = await getMovieData(props.query.q);
   return {
     props : {"results": data.data}
